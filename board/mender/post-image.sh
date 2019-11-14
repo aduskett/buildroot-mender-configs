75ae4ad22ef8fe978911ac6daca62275b54853ac
Adam Duskett <Aduskett@gmail.com>
2019-11-14 10:11:30 -0800

    initial commit, not complete

--- board/mender/post-image.sh
+++ board/mender/post-image.sh
@@ -33,30 +33,6 @@ trap 'rm -rf "${ROOTPATH_TMP}"' EXIT
 ROOTPATH_TMP="$(mktemp -d)"
 rm -rf "${GENIMAGE_TMP}"
 
-if [[ -e ${BINARIES_DIR}/u-boot.bin ]]; then
-  mv ${BINARIES_DIR}/u-boot.bin ${BINARIES_DIR}/kernel7l.img
-fi
-
-if [[ -e ${BINARIES_DIR}/config.txt ]]; then
-  rm -rf ${BINARIES_DIR}/config.txt
-fi
-
-if [[ -e ${BINARIES_DIR}/cmdline.txt ]]; then
-  rm -rf ${BINARIES_DIR}/cmdline.txt
-fi
-
-
-cat << __EOF__ >> "${BINARIES_DIR}/config.txt"
-dtoverlay=vc4-fkms-v3d
-dtoverlay=pi3-miniuart-bt
-enable_uart=1
-
-__EOF__
-
-cat << __EOF__ >> "${BINARIES_DIR}/cmdline.txt"
-root=\${mender_kernel_root} rootwait console=tty1 console=ttyAMA0,115200
-__EOF__
-
 function make_data_partition(){
   # Create the data partition
   if [[ -e ${BINARIES_DIR}/data-part.ext4 ]]; then
@@ -69,7 +45,6 @@ function make_data_partition(){
 
 
   mkdir -p ${BINARIES_DIR}/data_part
-  echo "device_type=raspberrypi4" > ${BINARIES_DIR}/data_part/device_type
   ${HOST_DIR}/sbin/mkfs.ext4 -d ${BINARIES_DIR}/data_part -r 1 -N 0 -m 5 -L "data" -O ^64bit ${BINARIES_DIR}/data-part.ext4 "${DATA_PART_SIZE}"
 }
 
@@ -96,6 +71,6 @@ genimage                           \
 
 
 # Uncomment this line to generate a mender artifact after the image is built.
-# create_mender_image "buildroot-rasperrypi-${BR2_VERSION}.mender"
+# create_mender_image "buildroot-nanopi-neo-core2-${BR2_VERSION}.mender"
 
 exit $?
